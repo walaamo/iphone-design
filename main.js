a43c8816cc1ddf417bbcc1375b8c3ad25b95569b
walaamo <100799835+walaamo@users.noreply.github.com>
2022-08-20 11:25:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img =document.querySelector('.img')
+let conrainer = document.querySelector('.container')
+
+function phones (phone){
+    img.src = phone
+}
+ 
+function colors (color){
+    conrainer.style.background =color
+}
